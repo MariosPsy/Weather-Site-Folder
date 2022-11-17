1ec1ed7a7eded9c6317de2f33be27798e7fc0edc
Marios <marious.ps@gmail.com>
2022-11-17 14:18:07 +0200

    Implement all working except inches

--- GUI/Exercise GUI/Exercise GUI.py
+++ GUI/Exercise GUI/Exercise GUI.py
@@ -6,8 +6,7 @@ inches_label = PySimpleGUI.Text("Enter inches: ")
 inches_input = PySimpleGUI.InputText(key="inches")
 convert_button = PySimpleGUI.Button("Convert")
 
-meters = "something"
-convert_label = PySimpleGUI.Text(meters, key="convert_label")
+convert_label = PySimpleGUI.Text("None", key="convert_label")
 
 window = PySimpleGUI.Window("Convertor",
                             layout=([feet_label, feet_input],
@@ -17,6 +16,13 @@ window = PySimpleGUI.Window("Convertor",
 while True:
     tuple = window.read()
     print("tuple: ", tuple)
+    if tuple[0] == PySimpleGUI.WINDOW_CLOSED :
+        break
+    elif tuple[0] == "Convert" :
+        float_feet = tuple[1]["feet"]
+        float_feet = float(float_feet.replace(",", "."))
+        result_feet = round((float_feet * 0.3048), 4)
+        window["convert_label"].update(value="{} m".format(result_feet))
 
 
 window.close()
