78d019215edda83818be9dbf0ead2e057fdf11b2
Marios <marious.ps@gmail.com>
2022-11-25 19:16:31 +0200

    PDF with difrent pages

--- PDF/main.py
+++ PDF/main.py
@@ -5,7 +5,7 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pandas.read_csv("topics.csv")
 
-print(df)
+#print(df)
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -14,6 +14,9 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"],
          align="L", ln=1)
     pdf.line(x1=10, y1=21, x2=200, y2=21) #Add an underline
+
+    for i in range(row["Pages"] - 1) :
+        pdf.add_page()
 
 
 pdf.output("csv_to_pdf.pdf")
