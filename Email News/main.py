e572c6992ca0e133161d0c74f534b0e3c67e00e5
Marios <marious.ps@gmail.com>
2022-11-30 13:42:08 +0200

    Add links and limitation of results, english only

--- Email News/main.py
+++ Email News/main.py
@@ -19,8 +19,11 @@ def email_senter(message) :
         server.sendmail(username, receiver, message)
 
 api = "b2c2b8a40ae144a28bf4a74f4390bdfc"
-url = """
-https://newsapi.org/v2/everything?q=tesla&from=2022-10-30&sortBy=publishedAt&apiKey=b2c2b8a40ae144a28bf4a74f4390bdfc"""
+topic = "tesla"
+url = f"https://newsapi.org/v2/everything?q={topic}" \
+      "&from=2022-10-30&sortBy=publishedAt" \
+      "&apiKey=b2c2b8a40ae144a28bf4a74f4390bdfc" \
+      "&Language=en"
 
 requests = requests.get(url)
 content = requests.json()
@@ -30,11 +33,13 @@ print(content)
 
 message = """Subject: Email News for Tesla""" + "\n"
 
-for article in content["articles"] :
+for article in content["articles"][0:5] :
     if (article["title"] is not None) and (article["description"] is not None) :
         # print(article["title"])
         # print(article["description"])
-        message = message + article["title"] + "\n" + article["description"] + "\n" + "\n"
+        message = message + article["title"] + "\n" \
+                + article["description"] + "\n" \
+                + article["url"]+ 2*"\n"
 
 print(message)
 
