eda378b841ad19fb72998cfeba0b4efa9448af70
Marios <marious.ps@gmail.com>
2022-11-30 12:53:30 +0200

    Sent mail and debug the proplems utf8 and None

--- Email News/main.py
+++ Email News/main.py
@@ -19,31 +19,26 @@ def email_senter(message) :
         server.sendmail(username, receiver, message)
 
 api = "b2c2b8a40ae144a28bf4a74f4390bdfc"
-url = """https://newsapi.org/v2/everything?q=tesla&from=2022-10-28&sortBy=publishedAt&apiKey=b2c2b8a40ae144a28bf4a74f4390bdfc"""
+url = """
+https://newsapi.org/v2/everything?q=tesla&from=2022-10-30&sortBy=publishedAt&apiKey=b2c2b8a40ae144a28bf4a74f4390bdfc"""
 
 requests = requests.get(url)
 content = requests.json()
 print(content)
 
-message = host = "smtp.gmail.com" #Default arguments for gmail
-port = 465
 
-username = "marious.ps@gmail.com"
-password = "ehopwycdwiekempo"
-
-receiver = "marious.ps@gmail.com"
-
-#Create a secure contex
-contex = ssl.create_default_context()
 
 message = """Subject: Email News for Tesla""" + "\n"
 
 for article in content["articles"] :
-    # print(article["title"])
-    # print(article["description"])
-    message += article["title"] + "\n" + article["description"] + "\n" + "\n"
+    if (article["title"] is not None) and (article["description"] is not None) :
+        # print(article["title"])
+        # print(article["description"])
+        message = message + article["title"] + "\n" + article["description"] + "\n" + "\n"
 
 print(message)
+
+message = message.encode("utf-8")
 
 email_senter(message)
 
