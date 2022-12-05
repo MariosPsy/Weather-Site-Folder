34069fcec05e9ee42dee9935ed79373c7b5a1285
Marios <marious.ps@gmail.com>
2022-12-05 18:44:00 +0200

    Read CSV

--- Weather API/main.py
+++ Weather API/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import pandas
+
+
 
 app = Flask(__name__)
 
@@ -7,11 +10,14 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<word>")
-def about(word):
-
-    return {"definition": word.upper(),
-            "word": word}
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    path = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pandas.read_csv(path, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[]
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
 if __name__ == "__main__" :
