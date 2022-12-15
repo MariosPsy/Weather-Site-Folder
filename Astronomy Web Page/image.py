3f6205faa2db38baa5ce56466d2d890cc03aeb8f
MariosPsy <120580161+MariosPsy@users.noreply.github.com>
2022-12-15 21:15:01 +0200

    Delete Astronomy Web Page directory

--- Astronomy Web Page/image.py
+++
@@ -1,9 +0,0 @@
-import requests
-import api
-
-print("\n" + "Hello from image downloader" + "\n")
-
-image_url = api.content["hdurl"]
-response = requests.get(image_url)
-with open("image.jpg", "wb") as file :
-    file.write(response.content)
